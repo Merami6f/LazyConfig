47ba0928a93bd1c377339c97143a70545c127633
Merami6f <feelmysoul49@gmail.com>
2024-09-05 10:58:01 +0700

    24090503 change obsidian dir

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -11,7 +11,7 @@ return {
       workspaces = {
         {
           name = "personal",
-          path = "~/Documents/Notes/"
+          path = "~/Notes/obsidian/"
         },
         { ui = { enable = true } }
       }
